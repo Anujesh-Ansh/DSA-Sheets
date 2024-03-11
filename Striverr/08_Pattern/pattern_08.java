d7b74687373612cc2db6bc309930faf183641171
Anujesh Ansh <anshanujesh@gmail.com>
2024-03-11 21:26:08 +0530

    Pattern 8

---
+++ Striverr/08_Pattern/pattern_08.java
@@ -0,0 +1,50 @@
+/*
+Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the Reverse N-Star Triangle.
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+*****
+ ***
+  *
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 20
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+*****
+ ***
+  *
+Explanation Of Sample Input 1 :
+The first row contains five stars.
+The second row contains one space, followed by three stars.
+The third row contains two spaces, followed by a star.
+Sample Input 2 :
+1
+Sample Output 2 :
+*
+
+ */
+import java.util.*;
+
+public class pattern_08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=n;i>=1;i--){
+            for(int j =1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ;j<=(2*i) -1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
