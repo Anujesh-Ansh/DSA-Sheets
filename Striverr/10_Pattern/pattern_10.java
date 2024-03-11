5c40bba79e1ad878f574676d3230d60ef030c50e
Anujesh Ansh <anshanujesh@gmail.com>
2024-03-11 21:41:43 +0530

    Pattern 10

---
+++ Striverr/10_Pattern/pattern_10.java
@@ -0,0 +1,41 @@
+/*
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Rotated Triangle.
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+*
+**
+***
+**
+*
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 20
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+*
+**
+***
+**
+*
+Sample Input 2 :
+1
+Sample Output 2 :
+*
+
+ */
+import java.util.*;
+
+public class pattern_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+
+        sc.close();
+    }
+}
